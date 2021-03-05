6fe851e3f5cf046d1f21f47844d264957380ac8b
mkoenke <mkoenke@sas.upenn.edu>
2021-03-05 14:34:33 -0500

    add list render to page

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import { useSelector } from 'react-redux'
 import { useActions } from '../hooks/useActions'
+import { useSelector } from '../hooks/useTypedSelector'
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('')
   const { searchRepositories } = useActions()
-  const { data, error, loading } = useSelector(
-    (state: any) => state.repositories
-  )
+  const { data, error, loading } = useSelector((state) => state.repositories)
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
@@ -19,6 +17,9 @@ const RepositoriesList: React.FC = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <li key={name}>{name}</li>)}
     </div>
   )
 }
